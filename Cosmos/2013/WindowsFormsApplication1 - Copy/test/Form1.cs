911d289c0861e63828a1ab377e8f0dc524494090
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-10-31 22:52:02 +0800

    up

---
+++ Cosmos/2013/WindowsFormsApplication1 - Copy/test/Form1.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            testEntities test = new testEntities();
+            var a = test.testtable1.Where(x => x.name == "eros").FirstOrDefault();
+            MessageBox.Show(a.name);
+        }
+    }
+}
