fe5643ab8131077b362b34bf80acca7d06366f8f
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-10-31 15:25:57 +0800

    up

---
+++ Cosmos/2013/WindowsFormsApplication1/test/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            testEntities test = new testEntities();
+            var a = test.testtable1.Where(x => x.name == "eros").ToList();
+
+        }
+    }
+}
--- Cosmos/Cosmos/MainPage.cs
+++ Cosmos/Cosmos/MainPage.cs
@@ -1,4 +1,6 @@
 ﻿using MetroFramework.Forms;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace Cosmos
 {
@@ -7,6 +9,14 @@ namespace Cosmos
         public MainPage()
         {
             InitializeComponent();
+            test();
+        }
+
+        public void test()
+        {
+            CosmosEntities cosmos = new CosmosEntities();
+            var a = cosmos.Tables.Where(x => x.sample == "eros").FirstOrDefault();
+            MessageBox.Show(a.sample);
         }
     }
 }
