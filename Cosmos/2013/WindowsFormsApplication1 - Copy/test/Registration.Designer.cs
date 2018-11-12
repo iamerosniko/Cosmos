0d443c7266776eacf768b9795b194e0901ce18b0
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-11-12 09:50:51 -0500

    up

--- Cosmos/2013/WindowsFormsApplication1 - Copy/test/Registration.Designer.cs
+++ Cosmos/2013/WindowsFormsApplication1 - Copy/test/Registration.Designer.cs
@@ -351,7 +351,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1362, 730);
+            this.ClientSize = new System.Drawing.Size(682, 730);
             this.ControlBox = false;
             this.Controls.Add(this.lblRegistrationName);
             this.Controls.Add(this.PnlMain);
